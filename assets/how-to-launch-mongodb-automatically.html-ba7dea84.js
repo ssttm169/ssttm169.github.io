7cb959a8fa062deb3ad66636acb37e0de48984d7
ssttm169 <ssttm169@users.noreply.github.com>
2023-07-01 05:01:57 +0000

    Auto deploy by the Vuepress-deploy

---
+++ assets/how-to-launch-mongodb-automatically.html-ba7dea84.js
@@ -0,0 +1,8 @@
+import{_ as e}from"./plugin-vue_export-helper-c27b6911.js";import{o as n,c as o,a as t,b as a,d as s}from"./app-3ea06b8d.js";const i={},d=a(`<p>在桌面上新建一个空的 启动MongoDB.bat , 输入以内命令:</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>D:
+cd D:\\mongodb\\bin
+net stop MongoDB
+net start MongoDB
+cd D:\\Program Files\\robo3
+start robo3t.exe
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,2),l=s("div",{class:"github-widget","data-repo":"ssttm169/launch_mongodb"},null,-1);function c(r,m){return n(),o("div",null,[d,t(" more "),l])}const b=e(i,[["render",c],["__file","how-to-launch-mongodb-automatically.html.vue"]]);export{b as default};
